0f6ea660acf66f3cda52bb1e958ed4abc0bb4e8b
RehkMansa <godspowernathaniel25@gmail.com>
2022-12-29 07:13:25 +0100

    create mixin for font-size

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,19 +1,22 @@
 import Illustration from "../../assets/login-illustration.svg";
 import { DefaultInput, PasswordInput } from "../../components/elements/Input";
+import styled from "./Login.module.scss";
 
 const Login = () => (
-    <main>
+    <main className={styled.main}>
         <img src={Illustration} alt="" />
-        <section>
-            <form>
-                <div>
-                    <h1>Welcome</h1>
+        <section className={styled.boxed}>
+            <div className="space-y-20">
+                <div className="space-y-4">
+                    <h1>Welcome!</h1>
                     <p>Enter details to login.</p>
                 </div>
-
-                <DefaultInput error="No username" />
-                <PasswordInput error="" />
-            </form>
+                <form className="space-y-6">
+                    <DefaultInput error="No username" />
+                    <PasswordInput error="" />
+                    <p className="text-accent tag">Forgot PASSWORD?</p>
+                </form>
+            </div>
         </section>
     </main>
 );
