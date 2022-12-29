3838402eed63218c0fccce90ff889f39a8cf2d0a
RehkMansa <godspowernathaniel25@gmail.com>
2022-12-29 02:30:37 +0100

    added pages

---
+++ src/pages/user/index.tsx
@@ -0,0 +1,7 @@
+const User = () => (
+    <div>
+        <h1>User Page</h1>
+    </div>
+);
+
+export default User;
---
+++ src/pages/user/userdetails/index.tsx
@@ -0,0 +1,7 @@
+const UserDetails = () => (
+    <div>
+        <h1>User Details</h1>
+    </div>
+);
+
+export default UserDetails;
